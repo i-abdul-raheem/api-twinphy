513e773e8250cffad86058fe78d9d30e9658c226
HassaanMujtaba <saddiquehari2511@gmail.com>
2023-08-16 07:07:30 -0700

    done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,10 @@
 const router = require('express').Router();
 const { auths } = require('./auth');
 const { users } = require('./user');
-const postRoutes= require('./posts');
 const {posts} = require('./posts');
 
 router.use('/auths', auths);
 router.use('/users', users);
-
-module.exports = { router };
-
-
-
 router.use('/posts', posts);
-router.use('/posts/:id', posts);
 
-module.exports= router;
+module.exports = router ;
