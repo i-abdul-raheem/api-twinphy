564e2fce872e21a8826aea027115187c8d71ed62
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-08-17 00:29:44 +0500

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,9 +5,9 @@ const { auth } = require('../middlewares');
 const handler = new User();
 
 router.get('/', handler.getUser);
-router.put('/:id', auth, handler.updateUser);
-router.patch('/:id', auth, handler.blockUser);
-router.delete('/:id', auth, handler.deleteUser);
+router.put('/:id', handler.updateUser);
+router.patch('/:id', handler.blockUser);
+router.delete('/:id', handler.deleteUser);
 
 module.exports = {
   users: router,
