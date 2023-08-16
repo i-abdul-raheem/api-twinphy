0e287c5b9cf5b25c8dc031ba8b0f970f93a0cf99
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-08-16 19:01:15 +0500

    Merged main

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,4 @@ const Post = require('./postModel');
 const { User } = require('./user.model');
 const { Chat } = require('./chat.model');
 
-
-module.exports={Post,
-  User,
-  Chat,
-};
+module.exports = { Post, User, Chat };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,11 @@ const router = require('express').Router();
 const { auths } = require('./auth');
 const { users } = require('./user');
 const chat = require('./chat');
-const postRoutes= require('./posts');
-const {posts} = require('./posts');
+const { posts } = require('./posts');
 
 router.use('/auths', auths);
 router.use('/users', users);
 router.use('/chat', chat);
+router.use('/posts', posts);
 
 module.exports = { router };
-
-
-
-router.use('/posts', posts);
-router.use('/posts/:id', posts);
-
-module.exports= router;
