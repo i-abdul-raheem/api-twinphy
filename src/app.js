bb5c40e44caab2f29a10cc29c0c964eb6e5acdc2
Muhammad Abuzar <abuzarrehan95@gmail.com>
2023-08-17 23:48:26 +0500

    google

--- src/app.js
+++ src/app.js
@@ -1,24 +1,90 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const passportSetup = require("./handlers/Passport");
 const app = express();
-const cors = require('cors');
-const { router } = require('./routes/index');
-const { db } = require('./db');
+const cors = require("cors");
+const { router } = require("./routes/index");
+const { db } = require("./db");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+// Set up session middleware
+app.use(
+  session({ secret: "hamza12345", resave: true, saveUninitialized: true })
+);
 
 app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(PORT, () => {
-  db.on('error', (err) => {
+  db.on("error", (err) => {
     console.log(err);
   });
-  db.on('open', () => {
-    console.log('Database Connected');
+  db.on("open", () => {
+    console.log("Database Connected");
     console.log(`Server Started: http://localhost:${PORT}`);
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// Initiate Google authentication
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+// Google callback URL
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      // Set a cookie with user data
+      res.cookie("userData", JSON.stringify(req.user, null, 5), { maxAge: 900000 }); // Set the cookie with a 15-minute expiration
+      res.redirect("http://localhost:3000/");
+    } else {
+      res.redirect("/login");
+    }
+  }
+);
+
+// Profile page (protected route)
+app.get("/profile", (req, res) => {
+  const userDataCookie = req.cookies.userData;
+
+  if (userDataCookie) {
+    const userData = JSON.parse(userDataCookie);
+    res.json(userData);
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
+// Logout
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.redirect("/auth/google");
 });
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,43 @@
-const router = require('express').Router();
-const { Auth } = require('../handlers');
+const router = require("express").Router();
+const { Auth } = require("../handlers");
+const passport = require("passport");
 
 const handler = new Auth();
 
-router.get('/', handler.refresh);
-router.post('/', handler.createProfile);
-router.post('/login', handler.login);
+router.get("/", handler.refresh);
+router.post("/", handler.createProfile);
+router.post("/login", handler.login);
+
+// router.get("/login/failed", (req, res) => {
+//   res.status(401).json({ error: true, message: "Log in failure" });
+// });
+
+// router.get("/login/success", (req, res) => {
+//   if (req.user) {
+//     res.status(200).json({
+//       error: false,
+//       message: "Successfully Loged In",
+//       user: req.user,
+//     });
+//   } else {
+//     res.status(403).json({ error: true, message: "Not Authorized" });
+//   }
+// });
+
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", {
+//     successRedirect: process.env.CLIENT_URL,
+//     failureRedirect: "/login/failed",
+//   })
+// );
+
+// router.get("/google", passport.authenticate("google",["profile","email"]));
+
+// router.get("/logout",(req,res)=>{
+//   req.logout();
+//   req.redirect(process.env.CLIENT_URL);
+// })
 
 module.exports = {
   auths: router,
