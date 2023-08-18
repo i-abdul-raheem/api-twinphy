27348e61fdf0b79e4618827292105b9a07091228
HassaanMujtaba <saddiquehari2511@gmail.com>
2023-08-18 11:01:19 -0700

    completed

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,5 +2,4 @@ const {Post}= require('./Post');
 const { User } = require('./User');
 const { Auth } = require('./Auth');
 const { Chat } = require('./Chat');
-const s3= require('./awsConfig');
-module.exports = { s3,User, Auth, Chat ,Post};
+module.exports = { User, Auth, Chat ,Post};
